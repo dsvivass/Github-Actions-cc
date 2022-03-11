941271b3217579f40a3daf76768dd97324c36473
dsvivass <dsvivass@unal.edu.co>
2022-03-10 19:30:09 -0500

    Unit tests workflow implemented

---
+++ main.py
@@ -0,0 +1,4 @@
+def main(case = None):
+    if case == 'saludar': return 'Hola mundo'
+    if case == 'despedir': return 'Adios'
+    return True
---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import unittest
+from main import main
+
+class MainTestCase(unittest.TestCase):
+    
+    def test_main(self):
+        '''Test de prueba para main'''
+        self.assertTrue(main())
+        
+    def test_saludo(self):
+        '''Test de prueba para saludo'''
+        self.assertEqual(main('saludar'), 'Hola mundo')
+    
+    def test_despedida(self):
+        '''Test de prueba para despedida'''
+        self.assertEqual(main('despedir'), 'Adios')
